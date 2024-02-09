eedce65766260680b441e4e51f1f4f3892af12ca
Mohammad Khlouf <mohammad.khlouf@gmail.com>
2024-02-09 17:41:03 +0200

    hi

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 //import Dotenv
-import 'dotenv/config';
+import {} from "dotenv/config";
 //import express
-import express  from 'express';
-import initApp from './src/modules/app.router.js';
+import express from "express";
+import initApp from "./src/modules/app.router.js";
 
-//create the app function 
-const app=express();
+//create the app function
+const app = express();
 // create a PORT variable and get the value from .env file or the default is 3000
-const PORT=process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-initApp(app,express);
+initApp(app, express);
 
-
-
-app.get('/',(req,res)=>{
-return res.json("OK");
+app.get("/", (req, res) => {
+  return res.json("OK");
 });
 
-
 // application listening details
-app.listen(PORT, ()=>{
-   console.log(`server is running on Port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`server is running on Port ${PORT}`);
 });
